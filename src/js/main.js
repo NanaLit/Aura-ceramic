e23dce673c3b093da4190f008552f487914acd25
NanaLit <nanalit16@gmail.com>
2023-08-08 07:26:57 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,74 @@
+"use strict";
+
+import mobileMenu from "./modules/mobile-menu";
+
+
+window.addEventListener("load", () => {
+  gsap.registerPlugin(ScrollToPlugin);
+  gsap.registerPlugin(ScrollTrigger);
+SmoothScroll({
+    // Время скролла 400 = 0.4 секунды
+    animationTime: 700,
+    // Размер шага в пикселях
+    stepSize: 100,
+
+    // Дополнительные настройки:
+
+    // Ускорение
+    accelerationDelta: 20,
+    // Максимальное ускорение
+    accelerationMax: 1,
+
+    // Поддержка клавиатуры
+    keyboardSupport: true,
+    // Шаг скролла стрелками на клавиатуре в пикселях
+    arrowScroll: 50,
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+
+    // Поддержка тачпада
+    touchpadSupport: true,
+  });
+  mobileMenu();
+  
+ 
+    // let form = document.getElementById("form"),
+    //     serverResponse = document.querySelector('.feedback__ok');
+
+    //   form.addEventListener("submit", async function (event) {
+    //       event.preventDefault();
+
+    //       let response = await fetch('/mailer/mail.php', {
+    //           method: 'POST',
+    //           body: new FormData(form)
+    //         });
+
+    //       if (response.ok) {
+    //           console.log("send");
+    //           form.classList.add('hidden')
+    //           serverResponse.style.display = "block"
+    //       } else {
+    //           console.log("error: " + response.status);
+    //           form.classList.add('hidden')
+    //           serverResponse.style.display = "block"
+    //           // serverResponse.innerHTML = "Message could not be sent"
+    //       }
+
+    //       form.reset();
+
+    //       serverResponse.addEventListener('click', function () {
+    //         form.classList.remove('hidden')
+    //         serverResponse.style.display = "none"
+    //       })
+
+      
+    // })
+
+
+
+
+});
---
+++ src/js/modules/mobile-menu.js
@@ -0,0 +1,56 @@
+function mobileMenu () {
+    const burger = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__menu');
+    const head = document.querySelector('.header__head');
+    const close = document.querySelector('.header__close');
+    const block = document.querySelector('.header__block');
+    const description = document.querySelector('.header__description');
+    const social = document.querySelector('.header__social');
+    const wrapper = document.querySelector('.header__wrapper');
+    const promoBlock = document.querySelector('.promo');
+    const body = document.querySelector('body');
+
+
+    const menuItems = document.querySelectorAll('.header__item')
+
+    function activeMenu() {
+        menu.classList.add('active');
+        burger.classList.add('active');
+        head.classList.add('active');
+        block.classList.add('active');
+        description.classList.add('active');
+        social.classList.add('active');
+        wrapper.classList.add('active');
+        promoBlock.classList.add('active');
+        body.classList.add('lock');
+    }
+
+    function disactiveMenu() {
+        menu.classList.remove('active');
+        burger.classList.remove('active');
+        head.classList.remove('active');
+        block.classList.remove('active');
+        description.classList.remove('active');
+        social.classList.remove('active');
+        wrapper.classList.remove('active');
+        promoBlock.classList.remove('active');
+        body.classList.remove('lock');
+    }
+
+    burger.addEventListener('click', () => {
+        activeMenu();
+    });
+
+    close.addEventListener('click', () => {
+        disactiveMenu();
+    })
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            disactiveMenu();
+        })
+    })
+
+}
+
+module.exports = mobileMenu;
