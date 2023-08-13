b78022886a959363edbf63ef8cfc904b9cf9c9d2
Negerev <nikita_holod@mail.ru>
2023-08-13 19:50:11 +0300

    added product-page

---
+++ src/js/productPage.js
@@ -0,0 +1,17 @@
+// "use strict";
+
+window.addEventListener("load", () => {
+
+  const swiper = new Swiper('.product__slider', {
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+    
+});
+
+
+// swiper.progress
